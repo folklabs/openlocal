50f0a8a00a78646f45fca049ceaa01c781c8d8e7
Paul Mackay <pauljmackay@gmail.com>
2015-04-06 22:32:41 +0100

    Add herne_hill_fdn theme.

---
+++ themes/herne_hill_fdn/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function herne_hill_fdn_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function herne_hill_fdn_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function herne_hill_fdn_preprocess_node(&$variables) {
+}
